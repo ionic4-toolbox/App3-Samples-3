20c9f8a70319f89bc82d2b3c7e94ee6dfa5318ad
Nono9515 <guevol900@gmail.com>
2018-06-21 12:36:58 -0500

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,179 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AlertController } from 'ionic-angular';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { AddRoomPage} from "../pages/add-room/add-room";
-import {CleaningPage} from "../pages/cleaning/cleaning";
-import {CommentsPage} from "../pages/comments/comments";
-import {ProfilePage} from "../pages/profile/profile";
-import {EmailProvider} from "../providers/email/email";
 
-import {File, IWriteOptions} from '@ionic-native/file';
-import {DatabaseProvider} from "../providers/database/database";
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+  rootPage:any = TabsPage;
 
-  rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
-  icons:any;
-  pages2: any;
-  constructor(public platform: Platform,
-              public statusBar: StatusBar,
-              public splashScreen: SplashScreen,
-              private _EMAIL       : EmailProvider,
-              public plt: Platform,
-              private file: File,
-              private databaseProvider: DatabaseProvider,
-              private alertCtrl: AlertController
-
-  ) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-
-    this.icons = ['md-bookmarks','md-home','ios-water"' ]
-    this.pages = [
-      { title: 'Inicio', component: HomePage},
-      { title: 'Añadir Habitación', component: AddRoomPage},
-      { title: 'Limpieza', component: CleaningPage}
-    ];
-    this.pages2 = new Array();
-    this.pages2 = {
-      HomePage: HomePage,
-      AddRoomPage: AddRoomPage,
-      CleaningPage: CleaningPage,
-      CommentsPage: CommentsPage,
-      ProfilePage: ProfilePage,
-
-    }
-
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
-
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
-
-  recomendation(){
-
-    // Retrieve the validated form fields
-    let message = 	"Prueba la aplicación Guudbed de gestión de reservas de alojamiento, que uso a diario. URL";
-
-    this._EMAIL.sendEmail('', "", "", "Agilice su gestión de reservas", message);
-
-  }
-
-
-  exportDataBase(){
-
-    this.plt.ready()
-      .then(() => {
-        let db = this.databaseProvider.exportAsSQL();
-        db.then(value => {
-
-          let date = new Date();
-          let today =date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getFullYear();
-          let name = "guudbed-"+today+".db";
-          let options: IWriteOptions = {
-            replace: true
-          }
-          let result = this.file.writeFile(this.file.externalRootDirectory, name, value.toString(), options)
-            .then(function (success) {
-
-                  }, e =>function (error) {
-
-                   });
-          let alert = this.alertCtrl.create({
-            title: 'Base de datos exportada',
-            subTitle: 'Su base de datos ha sido exportada y guardada en la raíz de almacenamiento de su memoria',
-            buttons: ['Aceptar']
-          });
-          alert.present();
-        });
-      })
-  }
-
-  importDataBase(){
-
-    this.plt.ready()
-      .then(() => {
-
-        let alert = this.alertCtrl.create({
-          title: 'Confirmar',
-          message: '¿Desea importar una nueva base de datos a Guudbed? El archivo debe estar en el directorio raíz ' +
-          'de la memoria interna de su dispositivo y llamarse "guudbed.db"',
-          buttons: [
-            {
-              text: 'Cancelar',
-              role: 'cancel',
-              handler: () => {
-                console.log('Cancel clicked');
-              }
-            },
-            {
-              text: 'Aceptar',
-              handler: () => {
-
-                let sql = "";
-                let f = this.file.readAsText(this.file.externalRootDirectory, "guudbed.db")
-                  .then(function (success) {
-                    sql = success;
-                  }, e => function (error) {
-
-                  });
-
-                f.then(value => {
-
-                  this.databaseProvider.importSQL(sql);
-                  let alert = this.alertCtrl.create({
-                    title: 'Base de datos importada',
-                    subTitle: 'Su base de datos ha sido importada con éxito',
-                    buttons: ['Aceptar']
-                  });
-                  alert.present();
-                })
-
-              }
-            }]
-        });
-        alert.present();
-
-   })
-  }
-
-
-  alertIcon(){
-
-    let alert = this.alertCtrl.create({
-      title: 'Contactar',
-      subTitle: '<br>E-mail: lbencomo94@nauta.cu <br><br>Celular: 58236723',
-      buttons: ['Aceptar']
-    });
-    alert.present();
-
-
-  }
-
-
-
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,107 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { AddRoomPage} from "../pages/add-room/add-room";
-import {CleaningPage} from "../pages/cleaning/cleaning";
-import {ClientePage} from "../pages/cliente/cliente";
-import {DetallesPage} from "../pages/detalles/detalles";
-import {TabPage} from "../pages/tab/tab";
-import {CommentsPage} from "../pages/comments/comments";
+import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Calendar } from '@ionic-native/calendar';
-
-import { AddEventPage } from '../pages/add-event/add-event';
-
-import { DatabaseProvider } from '../providers/database/database';
-import {IonicStorageModule} from "@ionic/storage";
-import {HttpModule} from "@angular/http";
-
-import {SQLitePorter} from "@ionic-native/sqlite-porter";
-import {SQLite} from "@ionic-native/sqlite";
-
-import { IonicSwipeAllModule } from 'ionic-swipe-all';
-
-import { NativePageTransitions } from '@ionic-native/native-page-transitions';
-
-import { ViewportModule } from 'angular2-viewport';
-
-import { InfiniteListModule } from 'angular-infinite-list'
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import {ServicioProvider} from "../providers/servicio/servicio";
-import {HttpClientModule} from "@angular/common/http";
-
-import {EmailComposer} from "@ionic-native/email-composer";
-import { EmailProvider } from '../providers/email/email';
-import {DatePicker} from "@ionic-native/date-picker";
-import { File } from '@ionic-native/file';
-import {FilterPage} from "../pages/filter/filter";
-import {ProfilePage} from "../pages/profile/profile";
-
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 @NgModule({
   declarations: [
     MyApp,
+    AboutPage,
+    ContactPage,
     HomePage,
-    ListPage,
-    AddEventPage,
-    AddRoomPage,
-    CleaningPage,
-    ClientePage,
-    DetallesPage,
-    TabPage,
-    CommentsPage,
-    FilterPage,
-    ProfilePage
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    IonicStorageModule.forRoot(),
-    IonicModule.forRoot(MyApp),
-    IonicSwipeAllModule,
-    ViewportModule,
-    InfiniteListModule,
-    InfiniteScrollModule,
-    HttpClientModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    AboutPage,
+    ContactPage,
     HomePage,
-    ListPage,
-    AddEventPage,
-    AddRoomPage,
-    CleaningPage,
-    ClientePage,
-    DetallesPage,
-    TabPage,
-    CommentsPage,
-    FilterPage,
-    ProfilePage
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    Calendar,
-    DatabaseProvider,
-    SQLite,
-    SQLitePorter,
-    NativePageTransitions,
-    ViewportModule,
-    ServicioProvider,
-    EmailComposer,
-    EmailProvider,
-    DatePicker,
-    File,
-    ScreenOrientation
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
